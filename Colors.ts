5fbdad99018d171900bb9cf2b957733ab13ebef6
weipeter <weihsiang.peter@gmail.com>
2025-11-04 10:17:39 +0800

    first commit

---
+++ Colors.ts
@@ -0,0 +1,30 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+export default {
+  primary: '#E96546',
+  yellow: '#FBB158',
+  green: '#96C0C0',
+  orange: '#EDC8B6',
+  grey: '#B8B8B8',
+  bgGray: '#e3e2e2',
+  blue: '#89A6C9',
+  dark: '#1A1A1A',
+  newPurple: '#B4BEF4',
+  bgPurple: '#d3dafc',
+  newYellow: '#FFCE93',
+  bgYellow: '#ffe0bb',
+  newGreen: '#7AD0B2',
+  bgGreen: '#a9ecd5',
+  newRed: '#F99999',
+  newBlue: '#98CCF0',
+  bgBlue2: "#bfe2fb",
+  bgBlue: "#f8fafc",
+  systemBlue: '#007AFF',
+
+  white: '#ffffff',
+  black: '#000000',
+  backgroundGray: "#F2F2F7",
+}
